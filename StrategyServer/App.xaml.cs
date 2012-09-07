e951c6a987f21e5ca5bf6a493dd79b5b8fe13a07
Jiří Bobrek <bobrekjiri@gmail.com>
2012-09-07 13:22:10 +0200

    Added classes
    Added basic classes for World and Server class

--- StrategyServer/App.xaml.cs
+++ StrategyServer/App.xaml.cs
@@ -7,9 +7,6 @@ using System.Windows;
 
 namespace StrategyServer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
---
+++ StrategyServer/World/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyServer
+{
+    [Serializable]
+    class World
+    {
+        public int Day { get; set; }
+        public byte Width { get; set; }
+        public byte Height { get; set; }
+        public Tile[,] Tiles { get; set; }
+        public List<Player> Players { get; set; }
+        public List<Village> Villages { get; set; }
+    }
+}
